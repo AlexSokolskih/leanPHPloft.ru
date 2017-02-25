d8d0c9a8ad4bb4ff60987fcda1987a8a5533ca17
AlexSokolskih <xaam1@ya.ru>
2017-02-25 13:12:42 +0300

    коммит

---
+++ homeWork1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 25.02.2017
+ * Time: 13:01
+ */
+
+/* Задание #1  */
+$name = "Александр";
+$age = '32';
+echo "Меня зовут: $name <br>";
+print "Мне $age лет <br>";
+
+/* Задание #2  */
+$picture = 80;
+$pictureFeltpen = 23;
+$picturePencil = 40;
+$picturePain = $picture - $pictureFeltpen - $picturePencil;
+echo "На школьной выставке $picture рисунков. $pictureFeltpen  из них выполнены фломастерами, $picturePencil карандашами, а остальные $picturePain красками <br>";
+
+
+
+
+
