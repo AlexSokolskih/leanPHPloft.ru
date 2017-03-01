fab941d94977cff04924774ea32fd0e758d12b66
AlexSokolskih <xaam1@ya.ru>
2017-03-01 15:38:02 +0300

    сделал третье задание

--- homeWork2.php
+++ homeWork2.php
@@ -52,24 +52,25 @@ $operands = [
 ];
 
 
-//вариант 1
+function applyOperation($arrayOperands, $functionForApply)
+{
+    $result = 0;
+    foreach ($arrayOperands as $key => $item) {
+        if (!is_numeric($item)) {
+            echo '<p>ошибка неправильный аргумент в массиве чисел:' . $item . '</p>';
+        }
+        if ($key == 0) {
+            $result = $item;
+        } else {
+            $result = $functionForApply ($result, $item);
+        }
+    }
+    return $result;
+}
+
 function mathOperation($arrayOperands, $operation)
 {
-    function applyOperation($arrayOperands, $functionForApply)
-    {
-        $result = 0;
-        foreach ($arrayOperands as $key => $item) {
-            if (!is_numeric($item)) {
-                echo '<p>ошибка неправильный аргумент в массиве чисел:' . $item . '</p>';
-            }
-            if ($key == 0) {
-                $result = $item;
-            } else {
-                $result = $functionForApply ($result, $item);
-            }
-        }
-        return $result;
-    }
+
 
     switch ($operation) {
         case '+':
@@ -93,9 +94,33 @@ function mathOperation($arrayOperands, $operation)
             }));
             break;
         default:
-            echo 'ошибка, неправильное арифметическое действие.';
+            echo '<p>ошибка, неправильное арифметическое действие.</p>';
             break;
     }
 }
 
 mathOperation($operands, '/');
+echo '<br>';
+
+/*
+ Задание #3
+
+Функция должна принимать переменное число аргументов.
+Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
+Остальные аргументы это целые и/или вещественные числа.
+ */
+
+function mathOperationPlus($operand)
+{
+    if (!($operand == '-' or $operand == '+' or $operand == '*' or $operand == '/')) {
+        echo '<p> ошибка в функции mathOperation2 неправильный первый аргумент </p>';
+        return;
+    }
+    for ($i = 1; $i < func_num_args(); $i++){
+        $arguments[]=func_get_arg($i);
+    }
+    echo '<p>Результат работы функции mathOperation2</p>';
+    mathOperation($arguments,$operand);
+}
+
+mathOperationPlus('+', 2, 5, 10);
