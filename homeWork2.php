8ed014d59eb880991c4f9a2acdc7befd724d0806
AlexSokolskih <xaam1@ya.ru>
2017-03-01 12:10:07 +0300

    сделал второе задание

--- homeWork2.php
+++ homeWork2.php
@@ -1,11 +1,13 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 /**
  * Created by PhpStorm.
  * User: sokolskih
  * Date: 28.02.2017
  * Time: 16:31
  */
-
 
 
 /*  задание №1*/
@@ -36,7 +38,64 @@ printParagrah($animals, true);
 
 /*  задание №2*/
 
-function mathOperation($arrayIndex, $operation){
+/*Функция должна принимать 2 параметра:
+массив чисел;
+строку, обозначающую арифметическое действие,    которое нужно выполнить со всеми элементами массива.
+Функция должна вывести результат на экран.
+Функция должна обрабатывать любой ввод, в том числе некорректный и выдавать сообщения об этом
+*/
+
+$operands = [
+    0 => 10,
+    1 => 2,
+    2 => 5,
+];
 
 
+//вариант 1
+function mathOperation($arrayOperands, $operation)
+{
+    function applyOperation($arrayOperands, $functionForApply)
+    {
+        $result = 0;
+        foreach ($arrayOperands as $key => $item) {
+            if (!is_numeric($item)) {
+                echo '<p>ошибка неправильный аргумент в массиве чисел:' . $item . '</p>';
+            }
+            if ($key == 0) {
+                $result = $item;
+            } else {
+                $result = $functionForApply ($result, $item);
+            }
+        }
+        return $result;
+    }
+
+    switch ($operation) {
+        case '+':
+            echo(applyOperation($arrayOperands, function ($arg1, $arg2) {
+                return ($arg1 + $arg2);
+            }));
+            break;
+        case '-':
+            echo(applyOperation($arrayOperands, function ($arg1, $arg2) {
+                return ($arg1 - $arg2);
+            }));
+            break;
+        case '*':
+            echo(applyOperation($arrayOperands, function ($arg1, $arg2) {
+                return ($arg1 * $arg2);
+            }));
+            break;
+        case '/':
+            echo(applyOperation($arrayOperands, function ($arg1, $arg2) {
+                return ($arg1 / $arg2);
+            }));
+            break;
+        default:
+            echo 'ошибка, неправильное арифметическое действие.';
+            break;
+    }
 }
+
+mathOperation($operands, '/');
